81dd70ed62cc7ab3b3fbbe53bbad42ff280309ed
Breno A <git@breno.tech>
2024-03-15 11:52:50 -0300

    Revert "fix: module-info"
    This reverts commit 6117dc636bd1393a0e8f441abc88b2b27c763c07.

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module io.github.brenoepics.fourier {
-		requires transitive javafx.controls;
-		requires transitive javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
 
 
-		opens io.github.brenoepics.fourier to javafx.fxml;
-		exports io.github.brenoepics.fourier;
-		exports io.github.brenoepics.fourier.calculator;
-		opens io.github.brenoepics.fourier.calculator to javafx.fxml;
-		exports io.github.brenoepics.fourier.ui;
-		opens io.github.brenoepics.fourier.ui to javafx.fxml;
+    opens io.github.brenoepics.fourier to javafx.fxml;
+    exports io.github.brenoepics.fourier;
+    exports io.github.brenoepics.fourier.calculator;
+    opens io.github.brenoepics.fourier.calculator to javafx.fxml;
+    exports io.github.brenoepics.fourier.ui;
+    opens io.github.brenoepics.fourier.ui to javafx.fxml;
 }
